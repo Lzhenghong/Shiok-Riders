fc070af77c41285f09e10f53f87daaf4e5ef6291
Lzhenghong <80509725+Lzhenghong@users.noreply.github.com>
2021-05-30 05:23:40 +0800

    Update HomeScreen.js

--- shiok/src/screens/HomeScreen.js
+++ shiok/src/screens/HomeScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, StyleSheet} from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import {Text} from 'react-native-elements';
+import MapView from '../components/Map';
+import {SafeAreaView} from 'react-navigation';
+import {Context as LocationContext} from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
 const HomeScreen = () => {
+    const {setLocation} = useContext(LocationContext);
+    const err = useLocation(setLocation);
     return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
+        <SafeAreaView forceInset = {{top: 'always'}}>
+            <View>
+                <Text>HomeScreen</Text>
+                <MapView /> 
+                {err ? <Text>Please enable location services</Text> : null}
+            </View>
+        </SafeAreaView>
     );
 };
+
+HomeScreen.navigationOptions = () => {
+    return {
+      header: () => false,
+    };
+  };
 
 const styles = StyleSheet.create({});
 
