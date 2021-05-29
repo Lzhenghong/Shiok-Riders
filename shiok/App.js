a09c5c232606df3f6cd06fbac7c4c11ba7922d7c
Lzhenghong <80509725+Lzhenghong@users.noreply.github.com>
2021-05-30 05:24:34 +0800

    Update App.js

--- shiok/App.js
+++ shiok/App.js
@@ -11,6 +11,7 @@ import HistoryScreen from './src/screens/HistoryScreen';
 import LoadingScreen from './src/screens/LoadingScreen';
 import {Provider as AuthProvider} from './src/context/AuthContext';
 import {setNavigator} from './src/navigationRef';
+import {Provider as LocationProvider} from './src/context/LocationContext';
 
 const switchNavigator = createSwitchNavigator({
   Loading: LoadingScreen,
@@ -33,12 +34,14 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <AuthProvider>
-      <App
-        ref={(navigator) => {
-          setNavigator(navigator);
-        }}
-      />
-    </AuthProvider>
+    <LocationProvider>
+      <AuthProvider>
+        <App
+          ref={(navigator) => {
+            setNavigator(navigator);
+          }}
+        />
+      </AuthProvider>
+    </LocationProvider>
   );
 };
