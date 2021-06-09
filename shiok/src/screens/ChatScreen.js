98b08187227bb18ee6b763d7951178785ee6c743
Lzhenghong <hong2288@gmail.com>
2021-06-10 03:10:47 +0800

    temp chat screen

--- shiok/src/screens/ChatScreen.js
+++ shiok/src/screens/ChatScreen.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
-import { Text, Header } from 'react-native-elements';
+import { Text, Header, Input, Button } from 'react-native-elements';
 import {Entypo} from '@expo/vector-icons';
+import Spacer from '../components/Spacer';
+import Communications from 'react-native-communications';
 
 const ChatScreen = () => {
+    const [hp, setHp] = useState('');
+
+    const checkNum = (input) => {
+        return !isNaN(input);
+    };
+
     return (
         <View>
             <Header 
                 backgroundColor = '#3EB489'
                 containerStyle = {styles.header}
                 centerComponent = {{text: 'Chats', style: {color: '#fff', fontSize: 20, fontWeight: 'bold'}}}
+            />
+            <Input 
+                label = 'Insert phone Number'
+                labelStyle = {{color:'#555353'}}
+                value = {hp}
+                onChangeText = {(newTerm) => {
+                    checkNum(newTerm) ? setHp(newTerm) : setHp('');
+                }}
+                autoCapitalize = 'none'
+                autoCorrect = {false}
+            />
+            <Spacer />
+            <Button 
+                title = 'Send Text'
+                buttonStyle = {styles.button}
+                onPress = {() => {
+                    hp ? Communications.text(hp, 'test shiok rider chat') : console.log('error sending text');
+                }}
             />
         </View>
     );
@@ -26,6 +52,10 @@ ChatScreen.navigationOptions = () => {
 const styles = StyleSheet.create({
     header: {
         marginBottom: 15
+    },
+    button: {
+        backgroundColor: '#FF8400',
+        borderRadius: 20
     }
 });
 
