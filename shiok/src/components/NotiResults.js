22965bd2c7726225ebde172047b6e70d38a39a8e
Lzhenghong <hong2288@gmail.com>
2021-06-25 03:11:42 +0800

    before searchapi abstract

--- shiok/src/components/NotiResults.js
+++ shiok/src/components/NotiResults.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import {ListItem, Button} from 'react-native-elements';
-import { MaterialIcons } from '@expo/vector-icons';
+import {ListItem} from 'react-native-elements';
+import { navigate } from "../navigationRef";
+
 
 const NotiResults = ({results}) => {
     console.log(results);
+
+    const subtitle = (item) => {
+        switch (item.type) {
+            case 'Offer':
+                return 'You have received an offer from';
+            case 'Accept':
+                return 'Your offer is accepted by';
+            case 'Reject':
+                return 'Your offer is rejected by';
+            default: 
+                return 'You have been added as a friend by';
+        }
+    };
+
+    const onPress = (item) => {
+        if (item.type == 'Offer') {
+            return () => navigate('OfferDecision', {item});
+        }
+    };  
+
     return (
         <View>
             {
@@ -13,9 +34,15 @@ const NotiResults = ({results}) => {
                         key = {item._id}
                         bottomDivider 
                         topDivide
+                        onPress = {onPress(item)}
                     >
                         <ListItem.Content>
-                            <ListItem.Title style = {{fontWeight: 'bold'}}>{item.type}</ListItem.Title>
+                            <ListItem.Title style = {{fontWeight: 'bold'}}>
+                                {item.type}
+                            </ListItem.Title>
+                            <ListItem.Subtitle>
+                                {`${subtitle(item)} ${item.sender.username}`}
+                            </ListItem.Subtitle>
                         </ListItem.Content>
                         <ListItem.Chevron />
                     </ListItem>
