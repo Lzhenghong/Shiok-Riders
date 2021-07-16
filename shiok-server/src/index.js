e43a141d848f0db17a0fef9c200d005b67011337
Vinleon <kohvinleon@gmail.com>
2021-07-16 09:29:10 +0800

    Change configuration for heroku deployment

--- shiok-server/src/index.js
+++ shiok-server/src/index.js
@@ -61,6 +61,6 @@ app.get("/", requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email}`);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
